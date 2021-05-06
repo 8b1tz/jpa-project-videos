ffa4faa4fcd62e2154c77d5eff82bde1f063342e
AnaJL <analinsdev@gmail.com>
2021-05-06 18:17:55 -0300

    mudando daoVdeo e daoUsuario

--- src/dao/DAOvideo.java
+++ src/dao/DAOvideo.java
@@ -10,39 +10,37 @@ import modelo.Usuario;
 import modelo.Video;
 
 public class DAOvideo extends DAO<Video> {
-	@Override
-	public Video read(Object chave) {
-		try {
-			String link = (String) chave;
-			TypedQuery<Video> q = manager.createQuery("select v from Video v where v.link=:l", Video.class);
-			q.setParameter("l", link);
-			return q.getSingleResult();
-		} catch (NoResultException e) {
-			return null;
-		}
-	}
+    @Override
+    public Video read(Object chave) {
+        try {
+            String link = (String) chave;
+            TypedQuery<Video> q = manager.createQuery("select v from Video v where v.link =: l", Video.class);
+            q.setParameter("l", link);
+            return q.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
 
+    public List<Video> consultarVideosPorAssunto(String palavra) {
+        try {
+            TypedQuery<Video> q = manager.createQuery("select v from Video v join v.assuntos a where a.palavra =:p",
+                    Video.class);
+            q.setParameter("p", palavra);
+            return q.getResultList();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
 
-
-	public List<Video> consultarVideosPorAssunto(String palavra) {
-		try {
-			TypedQuery<Video> q = manager.createQuery("select v from Video v join Assunto a where a.palavra=:p",
-					Video.class);
-			q.setParameter("p", palavra);
-			return q.getResultList();
-		} catch (NoResultException e) {
-			return null;
-		}
-	}
-
-	public List<Video> consultarVideosPorUsuario(String email) {
-		try {
-			TypedQuery<Video> q = manager.createQuery("select v from Video v join Usuario u where u.email=:e",
-					Video.class);
-			q.setParameter("e", email);
-			return q.getResultList();
-		} catch (NoResultException e) {
-			return null;
-		}
-	}
+    public List<Video> consultarVideosPorUsuario(String email) {
+        try {
+            TypedQuery<Video> q = manager.createQuery(
+                    "select v from Video v join v.visualizacoes vi join vi.usuario u where u.email=:e", Video.class);
+            q.setParameter("e", email);
+            return q.getResultList();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
 }
